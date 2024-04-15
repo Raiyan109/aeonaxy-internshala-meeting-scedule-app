ee89df6475c383b41ea584bd7f224b6c72aab508
Raiyan109 <kraiyan109@gmail.com>
2024-04-15 16:40:11 +0600

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,90 @@
-import { BiSearch } from 'react-icons/bi'
-import { GiHamburgerMenu } from 'react-icons/gi'
-import { BsFillPersonFill } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     return (
         <div>
-            <header className="text-gray-600 body-font">
-                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                        {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg> */}
-                        <span className="ml-3 text-xl">AirBnb</span>
-                    </a>
-                    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-                        <a className="mr-5 hover:text-gray-900">Any where</a>
-                        <a className="mr-5 hover:text-gray-900">Any week</a>
-                        <a className="mr-5 hover:text-gray-900">Add guest</a>
-                        <div><BiSearch /></div>
+            <header className="p-4 text-gray-800">
+                <div className="container flex justify-between h-16 mx-auto">
+                    <div className="flex">
+                        <a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2 text-3xl font-medium">
+                            Fibery
+                        </a>
+                        {/* Laptop screen */}
+                        <ul className="items-stretch hidden space-x-5 lg:flex">
+                            <li className="flex">
+                                <select name="cars" id="cars">
+                                    <option value="volvo">Solutions</option>
 
-                    </nav>
-                    <div className='flex justify-center items-center gap-2 border-gray-200 shadow-md p-3 rounded-xl'>
-                        <div><GiHamburgerMenu /></div>
-                        <Link to='/login'><BsFillPersonFill /></Link>
+                                </select>
+                            </li>
+                            <li className="flex">
+                                <select name="cars" id="cars">
+                                    <option value="volvo">Product</option>
+
+                                </select>
+                            </li>
+                            <li className="flex">
+                                <select name="cars" id="cars">
+                                    <option value="volvo">Resources</option>
+
+                                </select>
+                            </li>
+                            <li className="flex">
+                                <Link to='/' className='flex items-center px-4 md:px-1 -mb-1 border-b-2 border-transparent'>Pricing</Link>
+                            </li>
+                        </ul>
                     </div>
+                    <div className="items-center flex-shrink-0 hidden lg:flex gap-4">
+                        <button className="px-5 py-2 font-semibold rounded bg-transparent text-[#212529]">Login</button>
+                        <button className="px-5 py-2 font-semibold rounded bg-transparent border-[#212529] border text-[#212529]">Get a demo</button>
+                        <button className="px-5 py-2 font-semibold rounded bg-[#212529] text-gray-50">Sign up</button>
+                    </div>
+                    <button className="p-4 flex lg:hidden text-4xl absolute right-0 z-10"
+                        onClick={() => setOpen(!open)}
+                    >
+
+                        {open ? <IoMdClose /> :
+                            <RxHamburgerMenu />}
+                    </button>
+                    {open &&
+                        (
+                            <div className='absolute -right-5 md:-left-28 h-screen bg-teal-100 w-screen transition ease-out duration-100 transform'>
+                                {/* hidden lg: */}
+                                <ul className="space-y-5 flex flex-col items-center absolute left-44 md:left-96 top-52 text-2xl">
+                                    <li className="flex">
+                                        <select name="cars" id="cars">
+                                            <option value="volvo">Solutions</option>
+
+                                        </select>
+                                    </li>
+                                    <li className="flex">
+                                        <select name="cars" id="cars">
+                                            <option value="volvo">Product</option>
+
+                                        </select>
+                                    </li>
+                                    <li className="flex">
+                                        <select name="cars" id="cars">
+                                            <option value="volvo">Resources</option>
+
+                                        </select>
+                                    </li>
+                                    <li className="flex">
+                                        <Link to='/' className='flex items-center px-4 md:px-1 -mb-1 border-b-2 border-transparent'>Pricing</Link>
+                                    </li>
+
+                                </ul>
+                                <div className="flex flex-col items-center absolute flex-shrink-0 top-[550px] left-[170px] md:left-[380px] gap-3  lg:flex ">
+                                    <button className="px-5 py-2 font-semibold rounded bg-transparent text-[#212529]">Login</button>
+                                    <button className="px-5 py-2 font-semibold rounded bg-transparent border-[#212529] border text-[#212529]">Get a demo</button>
+                                    <button className="px-5 py-2 font-semibold rounded bg-[#212529] text-gray-50">Sign up</button>
+                                </div>
+                            </div>
+                        )}
                 </div>
             </header>
         </div>
